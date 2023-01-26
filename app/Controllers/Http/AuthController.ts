3929aac32b007c475415318934c7e080ccff61e9
Matheus Loukachak <matheus.augustolk@gmail.com>
2023-01-26 14:10:59 -0300

    feature: add eslint

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,13 +1,19 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import type { JWTTokenContract } from '@ioc:Adonis/Addons/Jwt';
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import type User from 'App/Models/User';
 
 export default class AuthController {
-  public async login({ auth, request }: HttpContextContract) {
+  public async login({ auth, request }: HttpContextContract): Promise<any> {
     const { email, password } = request.body();
-    return await auth.use('jwt').attempt(email, password);
+    return await auth.attempt(email, password);
   }
 
-  public async refresh({ auth, request }: HttpContextContract) {
+  public async refresh({
+    auth,
+    request,
+  }: HttpContextContract): Promise<JWTTokenContract<User>> {
     const { refreshToken } = request.body();
-    return await auth.use('jwt').loginViaRefreshToken(refreshToken);
+    const jwt = await auth.use('jwt').loginViaRefreshToken(refreshToken);
+    return jwt;
   }
 }
